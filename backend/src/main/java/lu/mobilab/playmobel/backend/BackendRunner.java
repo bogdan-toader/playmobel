8884e162d21e2dbf4eb086c4fb23bf1268bdd496
bogdan-uni <bogdan.toader@uni.lu>
2016-12-05 17:43:20 +0100

    first commit

---
+++ backend/src/main/java/lu/mobilab/playmobel/backend/BackendRunner.java
@@ -0,0 +1,87 @@
+package lu.mobilab.playmobel.backend;
+
+import org.mwg.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Created by bogdan.toader on 24/10/16.
+ */
+public class BackendRunner {
+
+
+    public void start() {
+
+        final Graph g = new GraphBuilder().withMemorySize(1000000).build();
+        g.connect(connectionResult -> {
+            Node user1 = g.newNode(0, 0);
+            g.index(0, 0, "users", new Callback<NodeIndex>() {
+                @Override
+                public void on(NodeIndex result) {
+                    result.addToIndex(user1);
+                }
+            });
+            String csvfile = "./bogdantoader.csv";
+            loadFromFile(csvfile, user1);
+
+
+
+            WSServer graphServer = new WSServer(g, 9011);
+            graphServer.start();
+
+            RESTManager rest = new RESTManager();
+            rest.start(g);
+
+//where is the paulwurt demo? please delete thefiles
+            //i think you deleted ls nop
+        });
+    }
+
+    private void loadFromFile(String csvfile, Node user) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(csvfile));
+            String line;
+            int count=0;
+            String[] attributes=null;
+            while ((line = br.readLine()) != null) {
+                String[] cells = line.split(";");
+                if(count==0){
+                    attributes=cells;
+                }
+                else {
+                    long time= Long.parseLong(cells[0]);
+
+                    String[] finalAttributes = attributes;
+                    user.travelInTime(time, new Callback<Node>() {
+                        @Override
+                        public void on(Node result) {
+                            for(int i = 1; i< cells.length; i++) {
+                                if (!cells[i].equals("")) {
+                                    try {
+                                        double d = Double.parseDouble(cells[i]);
+                                        result.set(finalAttributes[i], Type.DOUBLE, d);
+                                    } catch (Exception ex) {
+                                        //result.setProperty(finalAttributes[i], Type.STRING, cells[i]);
+                                    }
+                                }
+                            }
+                            result.free();
+                        }
+                    });
+                }
+                count++;
+            }
+            System.out.println("read: "+count+ " lines");
+        } catch (Exception ex) {
+        ex.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BackendRunner runner = new BackendRunner();
+        runner.start();
+    }
+
+}
---
+++ backend/src/main/java/lu/mobilab/playmobel/backend/RESTManager.java
@@ -0,0 +1,104 @@
+package lu.mobilab.playmobel.backend;
+
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import io.undertow.util.StatusCodes;
+import org.mwg.Graph;
+import org.mwg.task.ConditionalFunction;
+import org.mwg.task.Task;
+import org.mwg.task.TaskContext;
+
+import static org.mwg.core.task.Actions.*;
+
+
+/**
+ * Created by gnain on 17/10/16.
+ */
+public class RESTManager {
+
+    private Undertow server;
+    private Graph graph;
+
+
+    public void start(Graph graph) {
+        this.graph = graph;
+        if (server == null) {
+            server = Undertow.builder()
+                    .addHttpListener(8081, "0.0.0.0")
+                    .setHandler(Handlers.path()
+                            .addPrefixPath("/demo1", demo1Handler)
+                    )
+                    .build();
+        }
+        server.start();
+
+    }
+
+
+
+
+
+
+    private Task demo1Task = newTask()
+            .then(setTime("{{processTime}}"))
+            .then(readGlobalIndexAll("users"))
+            .then(setAsVar("users"))
+            .thenDo(context -> {
+                context.resultAsNodes().get(0).timepoints(org.mwg.Constants.BEGINNING_OF_TIME, org.mwg.Constants.END_OF_TIME,
+                        timepoints -> context.continueWith(context.wrap(timepoints)));
+            }).forEach(newTask()
+                    .then(setAsVar("timepoint"))
+                    .then(readVar("users"))
+                    .then(travelInTime("{{timepoint}}"))
+                    .then(attribute("{{attName}}"))
+                    .thenDo(context -> {
+                        try {
+
+                            long t = (long) context.variable("timepoint").get(0);
+                            double d = (double) context.result().get(0);
+
+                            if ((long) context.variable("timepoint").get(0) != org.mwg.Constants.BEGINNING_OF_TIME) {
+                                JsonObject serie = (JsonObject) context.variable("dataSet").get(0);
+
+                                ((JsonArray) serie.get("x")).add(t);
+                                ((JsonArray) serie.get("y")).add(d);
+                            }
+                        } catch (Exception ex) {
+
+                        }
+                        context.continueTask();
+                    })
+            );
+
+
+    private HttpHandler demo1Handler = new HttpHandler() {
+        @Override
+        public void handleRequest(HttpServerExchange httpServerExchange) throws Exception {
+            JsonArray result = new JsonArray();
+            JsonObject serie = new JsonObject();
+            serie.add("x", new JsonArray());
+            serie.add("y", new JsonArray());
+            result.add(serie);
+
+            TaskContext context = demo1Task.prepare(graph, null, taskResult -> {
+
+                httpServerExchange.getResponseHeaders().add(new HttpString("Access-Control-Allow-Origin"), "*");
+                //httpServerExchange.getResponseHeaders().add(new HttpString("Access-Control-Allow-Methods"), "GET, POST, PUT, DELETE");
+
+                httpServerExchange.setStatusCode(StatusCodes.OK);
+                httpServerExchange.getResponseSender().send(result.toString());
+            });
+            context.setVariable("processTime", System.currentTimeMillis());
+            context.setVariable("dataSet", serie);
+            context.setVariable("attName", httpServerExchange.getQueryParameters().get("attributeName").getFirst());
+            demo1Task.executeUsing(context);
+        }
+    };
+
+
+}
